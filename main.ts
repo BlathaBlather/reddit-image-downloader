a9e4a7451f83095810b5cd4bc65b3c7a6445771e
Michael Henson <32522738+BlathaBlather@users.noreply.github.com>
2022-02-28 17:54:03 +0000

    first commit

---
+++ main.ts
@@ -0,0 +1,33 @@
+// Import relevent libraries
+const RedditImageFetcher = require("reddit-image-fetcher");
+const imageDownloader = require('node-image-downloader')
+
+// Set number of images to download (max 50)
+const number_of_images = 1;
+
+// Choose the subreddits to get images from
+const subreddits = ["195", "196"];
+
+// Log to console fetching post urls
+console.log(`Fetching ${number_of_images} random photos from subreddits ${subreddits}.`)
+
+
+RedditImageFetcher.fetch({ // Fetch the image urls
+    type: 'custom',
+    total: number_of_images,
+    subreddit: subreddits
+})
+    .then(function (result: any[]) { // After fetching urls
+        console.log("Downloading image url(s)") // Log to console downloading images
+        var imgs = []
+        for (let i = 0; i < result.length; i++) {
+            imgs.push({ uri: result[i]["image"] }); // For each url, append to array
+        }
+        return imageDownloader({ // Download all the images
+            imgs: imgs,
+            dest: 'img', // Destination folder
+        })
+    })
+    .then(() => {
+        console.log("Finished.") // Log to console finished
+    })
---
+++ readme.md
@@ -0,0 +1,8 @@
+## Download random images from mutiple subreddits
+
+Change the number of images to download and subreddits to view with the constants at the top of the page.
+
+Requires 
+
+```reddit-image-fetcher
+node-image-downloader```
